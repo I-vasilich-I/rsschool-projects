9357b39296e6dc74cc29b698c5565f54fb8f1d1a
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-10-21 01:05:05 +0300

    fix: mobile menu behaver js

--- shelter/pages/script.js
+++ shelter/pages/script.js
@@ -3,10 +3,9 @@ window.onload = function() {
     resetClass();
     
 }
-let count = 0;
+
 // runs on both pages: main & pets
 function onClickMenu() {
-    count = 0;
     let blackout = document.querySelector('.blackout');
     if (blackout!=null) {
        document.body.removeChild(blackout);
@@ -20,18 +19,20 @@ function onClickMenu() {
     if (mainBurger!=null) {
         mainBurger.classList.toggle('burger-menu');
     }
+
     let petsBurger = document.getElementById('burger-2');
     if (petsBurger != null) {
         petsBurger.classList.toggle('burger-menu');
         petsBurger.classList.toggle('burger-pets');
     }
+
     document.querySelector('.mobile__menu').classList.toggle('mobile__menu-toggled');
-    //document.querySelector('.blackout').classList.toggle('blackout-active');
     document.querySelector('.header__logo').classList.toggle('header__logo-toggled');
     let headerPets = document.querySelector('.header--pets');
     if (headerPets != null) {
         headerPets.classList.toggle('header--pets-toggled');
     }
+
     let burgerLine = document.getElementById('burger__line');
     if (burgerLine != null) {
         burgerLine.classList.toggle('burger__line-pets');
@@ -42,31 +43,32 @@ function onClickMenu() {
 function resetClass() {
     window.addEventListener("resize", () => {
         if(window.innerWidth > 767) {
-            if (count) return;
-            count++;
             let mainBurger = document.getElementById('burger-1');
             if (mainBurger!=null) {
                 mainBurger.classList.remove('burger-menu');
             }
+
             let petsBurger = document.getElementById('burger-2');
             if (petsBurger != null) {
                 petsBurger.classList.remove('burger-menu');
                 petsBurger.classList.add('burger-pets');
             }
+
             document.querySelector('.mobile__menu').classList.remove('mobile__menu-toggled');
-            //document.querySelector('.blackout').classList.remove('blackout-active');
             let blackout = document.querySelector('.blackout');
             if (blackout!=null) {
                document.body.removeChild(blackout);
             }
+
             document.querySelector('.header__logo').classList.remove('header__logo-toggled'); 
             let headerPets = document.querySelector('.header--pets');
             if (headerPets != null) {
-                headerPets.classList.toggle('header--pets-toggled');
+                headerPets.classList.remove('header--pets-toggled');
             }
+            
             let burgerLine = document.getElementById('burger__line');
             if (burgerLine != null) {
-                burgerLine.classList.toggle('burger__line-pets');
+                burgerLine.classList.add('burger__line-pets');
             }
         }
     })
