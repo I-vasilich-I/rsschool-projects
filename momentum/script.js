30da762281d5bec0e1f474cae5d309b2abcbf5d8
Oleg-Vaskevich <oleg_vas08@mail.ru>
2020-10-20 16:44:03 +0300

    feat: get & set Name & Focus

--- momentum/script.js
+++ momentum/script.js
@@ -7,7 +7,8 @@ const greeting = document.querySelector('.greeting');
 
 // Months&Days
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+                'August', 'September', 'October', 'November', 'December'];
 
 // Show Time
 function showTime() {
@@ -34,25 +35,25 @@ function addZero(num) {
     return num < 10 ? `0${num}` : num;
 }
 
-function setBackground() {
+function setBackgroundAndGreeting() {
     let today = new Date();
     let hour = today.getHours();
     if (hour < 6) {
-        let path = './assets/images/night';
-        document.body.style.backgroundImage = `url(${path}/${addZero(getRandomInt(20)+1)}.jpg)`;
+        let path = './assets/images/night/';
+        document.body.style.backgroundImage = `url(${path}${addZero(getRandomInt(19)+1)}.jpg)`;
         greeting.textContent = 'Good Night, ';
     } else if (hour < 12) {
-        let path = './assets/images/morning';
-        document.body.style.backgroundImage = `url(${path}/${addZero(getRandomInt(20)+1)}.jpg)`;
+        let path = './assets/images/morning/';
+        document.body.style.backgroundImage = `url(${path}${addZero(getRandomInt(19)+1)}.jpg)`;
         greeting.textContent = 'Good Morning, ';
     } else if (hour < 18) {
-        let path = './assets/images/day';
-        document.body.style.backgroundImage = `url(${path}/${addZero(getRandomInt(20)+1)}.jpg)`;
+        let path = './assets/images/day/';
+        document.body.style.backgroundImage = `url(${path}${addZero(getRandomInt(19)+1)}.jpg)`;
         document.body.style.color = 'black';
         greeting.textContent = 'Good Afternoon, ';
     } else {
-        let path = './assets/images/evening';
-        document.body.style.backgroundImage = `url(${path}/${addZero(getRandomInt(20)+1)}.jpg)`;
+        let path = './assets/images/evening/';
+        document.body.style.backgroundImage = `url(${path}${addZero(getRandomInt(19)+1)}.jpg)`;
         document.body.style.color = 'white';
         greeting.textContent = 'Good Evening, ';
     }
@@ -63,5 +64,51 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+function setName(e) {
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
+//Run
 showTime();
-setBackground();
+setBackgroundAndGreeting();
+getName();
+getFocus();
