5ca761aa18f6032cb4829b8ea80dbed1bf171408
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-10-31 15:01:01 +0300

    feat: implement switch language, animation, etc

--- virtual-keyboard/js/Keyboard.js
+++ virtual-keyboard/js/Keyboard.js
@@ -40,7 +40,81 @@ export default class keyboard {
           this.keyButtons.push(keyButton);
           rowElement.appendChild(keyButton.div);
         }
-      })
+      });
+    });
+
+    document.addEventListener('keydown', this.handleEvent);
+    document.addEventListener('keyup', this.handleEvent);
+    document.addEventListener('mousedown', this.handleEvent);
+    document.addEventListener('mouseup', this.handleEvent);
+  }
+
+  handleEvent = (e) => {
+    if (e.stopPropagation) e.stopPropagation();
+    const { code, type } = e;
+    const keyObj = this.keyButtons.find((key) => key.code === code);
+    if (!keyObj) return;
+    this.output.focus();
+
+    if (type.match(/keydown|mousedown/)) {
+      if (type.match(/key/)) e.preventDefault();
+     
+      if (code.match(/Shift/)) this.shiftKey = true;
+
+      keyObj.div.classList.add('active');
+
+      // switch language
+      if (code.match(/Control/)) this.ctrlKey = true;
+      if (code.match(/Alt/)) this.altKey = true;
+
+      if (code.match(/Control/) && this.altKey) this.switchLanguage(); 
+      if (code.match(/Alt/) && this.ctrlKey) this.switchLanguage(); 
+
+
+
+      if (!this.isCaps) {
+        this.printToOutput(keyObj, this.shiftKey ? keyObj.shift : keyObj.small);
+      } else if (this.isCaps) {
+        if (this.shiftKey) {
+          this.printToOutput(keyObj, keyObj.sub.innerHTML ? keyObj.shift : keyObj.small);
+        } else {
+          this.printToOutput(keyObj, !keyObj.sub.innerHTML ? keyObj.shift : keyObj.small);
+        }
+      }
+      //release button
+    } else if (type.match(/keyup|mouseup/)) {
+      keyObj.div.classList.remove('active');
+
+      if (code.match(/Control/)) this.ctrlKey = false;
+      if (code.match(/Alt/)) this.altKey = false;
+      if (code.match(/Shift/)) this.shiftKey = false;
+    }
+  }
+
+  switchLanguage = () => {
+    const langAbbr = Object.keys(language);
+    let langIdx = langAbbr.indexOf(this.container.dataset.language);
+    this.keyBase = langIdx + 1 < langAbbr.length ? language[langAbbr[langIdx+=1]] 
+      : language[langAbbr[langIdx -= langIdx]];
+
+    this.container.dataset.language = langAbbr[langIdx];
+    storage.set('kbLang', langAbbr[langIdx]);
+
+    this.keyButtons.forEach((button) => {
+      const keyObj = this.keyBase.find((key) => key.code === button.code);
+      if (!keyObj) return;
+      button.shift = keyObj.shift;
+      button.small = keyObj.small;
+      if (keyObj.shift && keyObj.shift.match(/[^a-zA-zа-яА-ЯёЁ0-9]/g)) {
+        button.sub.innerHTML = keyObj.shift;
+      } else {
+        button.sub.innerHTML = '';
+      }
+      button.letter.innerHTML = keyObj.small;
     })
+  }
+
+  printToOutput(keyObj, symbol) {
+    console.log(symbol);
   }
 }
